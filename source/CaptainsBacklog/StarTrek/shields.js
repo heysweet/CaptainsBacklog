50fd82a2f438cc99fd35f6e2502c4efff52f8efc
Sweet, Andrew <Andrew.Sweet@nike.com>
2015-08-18 17:03:13 -0700

    Began transferring energy to the shields

--- source/CaptainsBacklog/StarTrek/shields.js
+++ source/CaptainsBacklog/StarTrek/shields.js
@@ -1,7 +1,14 @@
+Ship = function(){
+	var energy = 20000;
+}
+
 Shields = function(){
 	var isOnline = false;
+	var energy = 4000;
 
 	// Define variables and setters and getters
+
+	/* isOnline */
 
 	this.isOnline = function () {
 		return isOnline;
@@ -9,6 +16,12 @@ Shields = function(){
 
 	this.setOnline = function (state) {
 		isOnline = state;
+	}
+
+	/* energy */
+
+	this.getEnergy = function () {
+		return energy;
 	}
 }
 
--- source/CaptainsBacklog/Tests/shields.test.js
+++ source/CaptainsBacklog/Tests/shields.test.js
@@ -1,29 +1,47 @@
 describe('shields', function() {
-    
-    // beforeEach(function() {
-    // });
 
-    describe('shields should go up and down', function() {
-        // beforeEach(function() {
-        //     var distanceWhereRandomFactorsHoldSway = 3000;
-        //     ui.target = new Klingon(distanceWhereRandomFactorsHoldSway, 200);
+  describe('shields should go up and down', function() {
+    var shields;
 
-        //     game.processCommand(ui);
-        // });
-
-        it('shield should default to powered down', function() {
-            var shields = new Shields();
-
-            expect(shields.isOnline()).toBe(false);
-        });
-
-        it('shield should be able to turn on', function() {
-            var shields = new Shields();
-
-            shields.setOnline(true);
-
-            expect(shields.isOnline()).toBe(true);
-        });
+    beforeEach(function() {
+      shields = new Shields();
     });
+
+    it('shields should default to powered down', function() {
+      expect(shields.isOnline()).toBe(false);
+    });
+
+    it('shields should be able to turn on', function() {
+      shields.setOnline(true);
+
+      expect(shields.isOnline()).toBe(true);
+    });
+
+    it('shields should be able to turn off', function() {
+      shields.setOnline(true);
+
+      shields.setOnline(false);
+
+      expect(shields.isOnline()).toBe(false);
+    });
+  });
+
+  describe('can transfer energy to and from the shields', function() {
+    var shields;
+
+    beforeEach(function() {
+      shields = new Shields();
+    });
+
+    it('when shields first turn on, they start with 4000 energy', function() {
+      shields.setOnline(true);
+
+      expect(shields.getEnergy()).toBe(4000);
+    });
+
+    it('energy can be transferred from the ship to the shield', function() {
+      
+    });
+  });
 });
 
